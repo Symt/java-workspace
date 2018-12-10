248f8e3f4eb1c5b895516372f16c4485f4aa3c24
symt <xxrequired@gmail.com>
2018-12-10 08:12:08 -0600

    Update 1 (12/10/18)

--- cookie-clicker/src/main/CookieFrameMain.java
+++ cookie-clicker/src/main/CookieFrameMain.java
@@ -4,14 +4,21 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 
 public class CookieFrameMain extends JFrame {
+	BufferedImage cookie;
 	private static final long serialVersionUID = 1L;
-	PanelWrapper pw = new PanelWrapper();
+	PanelWrapper pw;
 	boolean click = false;
 	int cookies = 0;
+	double cps = 0;
+
 	public CookieFrameMain() {
 		addMouseListener((MouseListener) new MouseListener() {
 
@@ -37,11 +44,17 @@ public class CookieFrameMain extends JFrame {
 			@Override
 			public void mouseReleased(MouseEvent arg0) {
 			}
-			
+
 		});
 		setSize(1440, 822);
 		setResizable(false);
-		pw.setSize(getWidth()-20, getHeight());
+		try {
+			cookie = ImageIO.read(new File("cookie-2.png"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		pw = new PanelWrapper(cookie);
+		pw.setSize(getWidth() - 20, getHeight());
 		pw.setBackground(Color.BLACK);
 		add(pw);
 		setVisible(true);
@@ -51,29 +64,46 @@ public class CookieFrameMain extends JFrame {
 	public static void main(String[] args) {
 		new CookieFrameMain();
 	}
-	
+
 	public void run() {
-		long nextTime = System.currentTimeMillis()+50;
+		int lastSessionCookies = 0;
+		int sessionCookies = 0;
+		long startTime = System.currentTimeMillis() - 1000;
+		long nextTime = System.currentTimeMillis() + 50;
+		long secondTime = System.currentTimeMillis() + 1000;
 		while (true) {
 			if (nextTime - System.currentTimeMillis() < 0) {
-				nextTime = System.currentTimeMillis()+50;
+				nextTime = System.currentTimeMillis() + 50;
 				if (click) {
-					pw.setCircleSize(190, 190);
+					pw.setCircleSize(190, 190, cookies);
 					click = false;
 					cookies++;
+					sessionCookies++;
 				} else {
-					pw.setCircleSize(200, 200);
+					pw.setCircleSize(200, 200, cookies);
 				}
-				
-				repaint();
+				if (sessionCookies != lastSessionCookies) {
+					cps = (double) (sessionCookies
+							/ (double) ((System.currentTimeMillis() - startTime) / (double) 1000));
+					secondTime = System.currentTimeMillis() + 1000;
+				} else if (secondTime - System.currentTimeMillis() < 0) {
+					startTime = System.currentTimeMillis() - 1000;
+					sessionCookies = 0;
+					lastSessionCookies = -1;
+					cps = 0;
+				}
+				lastSessionCookies = sessionCookies;
+				pw.setCPS(cps);
+				pw.repaint();
 			}
 		}
 	}
-	
+
 	public boolean inCircle(int x, int y) {
-		double dist = Math.sqrt(Math.pow((x-(getWidth()/2)), 2) + Math.pow((y-(getHeight()/2+10)), 2));
-		return (dist < pw.width/2);
+		double dist = Math.sqrt(Math.pow((x - (getWidth() / 2)), 2) + Math.pow((y - (getHeight() / 2 + 10)), 2));
+		return (dist < pw.width / 2);
 	}
+
 	public void paint(Graphics g) {
 		super.paint(g);
 		pw.repaint();
--- cookie-clicker/src/main/PanelWrapper.java
+++ cookie-clicker/src/main/PanelWrapper.java
@@ -1,30 +1,49 @@
 package main;
 
-import java.awt.BasicStroke;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.math.RoundingMode;
+import java.text.DecimalFormat;
 
 import javax.swing.JPanel;
 
 public class PanelWrapper extends JPanel {
 	private static final long serialVersionUID = 1L;
-	int width, height;
-	public PanelWrapper() {
+	int width, height, totalCookies;
+	BufferedImage cookie;
+	String string, string2;
+	double cps;
+	DecimalFormat df = new DecimalFormat("#.###");
+
+	public PanelWrapper(BufferedImage cookie) {
 		width = 200;
 		height = 200;
+		this.cookie = cookie;
+		totalCookies = 0;
+		cps = 0.0;
+		df.setRoundingMode(RoundingMode.CEILING);
 	}
-	
-	public void setCircleSize(int w, int h) {
+
+	public void setCircleSize(int w, int h, int cookies) {
 		width = w;
 		height = h;
+		totalCookies = cookies;
+	}
+	
+	public void setCPS(double cps) {
+		this.cps = cps;
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		Graphics2D g2d = (Graphics2D) g;
-		g2d.setColor(Color.WHITE);
-		g2d.setStroke(new BasicStroke(2));
-		g2d.drawOval(getWidth()/2-(width/2), getHeight()/2-(height/2), width, height);
+		g.drawImage(cookie, getWidth() / 2 - (width / 2), getHeight() / 2 - (height / 2), width, height, null);
+		g.setColor(Color.WHITE);
+		g.setFont(new Font("TrebuchetMS", Font.PLAIN, 32)); 
+		string = "" + totalCookies;
+		string2 = "CPS: " + df.format(cps);
+		g.drawString(string, getWidth() / 2 - g.getFontMetrics().stringWidth(string)/2, getHeight() / 2 + 300);
+		g.drawString(string2, getWidth() / 2 - g.getFontMetrics().stringWidth(string2)/2, getHeight() / 2 - 300);
 	}
 }
