2ce6e9c07b9e4612be988b357b32bb9fbd530b79
symt <xxrequired@gmail.com>
2018-12-13 12:20:33 -0600

    Update 5 (12/13/18)

--- cookie-clicker/src/main/CookieFrameMain.java
+++ cookie-clicker/src/main/CookieFrameMain.java
@@ -11,6 +11,7 @@ import java.awt.event.WindowEvent;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
 
 import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
@@ -25,97 +26,19 @@ public class CookieFrameMain extends JFrame {
 	private static final long serialVersionUID = 1L;
 	PanelWrapper pw;
 	boolean click = false;
-	int cookies = 0;
 	double cps = 0;
 	double basecps = 0;
 	int multiplier = 1;
 	int totalCookies = 0;
-	JFrame shop = new JFrame("Shop");
+	JFrame shop;
+	JFrame powerup;
 	JLabel cookieLabel;
+	int slavesPerMinute = 0;
+	BigInteger cookies = BigInteger.valueOf(0);
 
 	public CookieFrameMain() {
 		setIconImage(new ImageIcon("icon.png").getImage());
-		shop.setTitle("Shop");
-		shop.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-		shop.setSize(1440, 822);
-		shop.setResizable(false);
-		shop.setBackground(new Color(34, 34, 34));
-		shop.setLayout(new GridLayout(3, 6));
-		JButton[] shopItems = { new JButton("Slave ( " + 100 + " )"),
-				new JButton("Indentured Servant ( " + 1000 + " )"), new JButton("Sharecropper ( " + 10000 + " )"),
-				new JButton("Overseer ( " + 100000 + " )") };
-
-		for (int i = 0; i < shopItems.length; i++) {
-			shopItems[i].setFocusable(false);
-			shopItems[i].addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
-					String sourceText = ((JButton) e.getSource()).getText();
-					String[] sourceInfo = sourceText.replaceAll("\\( (.*?) \\)", "-$1").replaceAll(" ", "").split("-");
-					int cookieCost = Integer.valueOf(sourceInfo[1]);
-					String cookieName = sourceInfo[0];
-
-					switch (cookieName.toLowerCase()) {
-					case "slave":
-						if (cookies >= cookieCost) {
-							cookies -= cookieCost;
-							basecps += 3;
-							cookieCost += cookieCost / 4;
-						}
-						((JButton) e.getSource()).setText("Slave ( " + cookieCost + " )");
-						break;
-					case "indenturedservant":
-						if (cookies >= cookieCost) {
-							cookies -= cookieCost;
-							basecps += 20;
-							cookieCost += cookieCost / 8;
-						}
-						((JButton) e.getSource()).setText("Indentured Servant ( " + cookieCost + " )");
-						break;
-					case "sharecropper":
-						if (cookies >= cookieCost) {
-							cookies -= cookieCost;
-							basecps += 200;
-							cookieCost += cookieCost / 16;
-						}
-						((JButton) e.getSource()).setText("Sharecropper ( " + cookieCost + " )");
-						break;
-					case "overseer":
-						if (cookies >= cookieCost) {
-							cookies -= cookieCost;
-							basecps += 1000;
-							cookieCost += cookieCost / 32;
-						}
-						((JButton) e.getSource()).setText("Overseer ( " + cookieCost + " )");
-						break;
-					}
-				}
-			});
-		}
-		boolean done = false;
-		JLabel l = new JLabel("Cookies: ", SwingConstants.RIGHT);
-		cookieLabel = new JLabel(" " + cookies, SwingConstants.LEFT);
-		for (int i = 0; i < 7; i++) {
-			shop.add(new JPanel());
-			if (i == 1 && !done) {
-				shop.add(l);
-				shop.add(cookieLabel);
-				i+=2;
-			}
-			if (i == 6 && !done) {
-				for (JButton k : shopItems)
-					shop.add(k);
-				i = 0;
-				done = true;
-			}
-		}
-
-		shop.addWindowListener(new java.awt.event.WindowAdapter() {
-			@Override
-			public void windowClosing(WindowEvent windowEvent) {
-				shop.setVisible(false);
-				setVisible(true);
-			}
-		});
+		shop = initShop();
 		addMouseListener(new MouseListener() {
 
 			@Override
@@ -125,24 +48,23 @@ public class CookieFrameMain extends JFrame {
 				} else if (inShopButton(arg0.getX(), arg0.getY())) {
 					setVisible(false);
 					shop.setVisible(true);
+				} else if (inPowerupButton(arg0.getX(), arg0.getY())) {
+					// setVisible(false);
+					// powerup.setVisible(true);
 				}
 			}
 
 			@Override
-			public void mouseEntered(MouseEvent arg0) {
-			}
+			public void mouseEntered(MouseEvent arg0) {}
 
 			@Override
-			public void mouseExited(MouseEvent arg0) {
-			}
+			public void mouseExited(MouseEvent arg0) {}
 
 			@Override
-			public void mousePressed(MouseEvent arg0) {
-			}
+			public void mousePressed(MouseEvent arg0) {}
 
 			@Override
-			public void mouseReleased(MouseEvent arg0) {
-			}
+			public void mouseReleased(MouseEvent arg0) {}
 
 		});
 		setSize(1440, 822);
@@ -172,17 +94,18 @@ public class CookieFrameMain extends JFrame {
 		long nextTime = System.currentTimeMillis() + 50;
 		long secondTime = System.currentTimeMillis() + 1000;
 		long time = System.currentTimeMillis() + 1000;
+		long minuteTime = System.currentTimeMillis() + 64000;
 		while (true) {
 			if (nextTime - System.currentTimeMillis() < 0) {
 				nextTime = System.currentTimeMillis() + 50;
 				if (click) {
 					pw.setCircleSize(190, 190, cookies);
 					click = false;
-					cookies += multiplier;
+					cookies = cookies.add(BigInteger.valueOf(multiplier));
 					sessionCookies += multiplier;
 					totalCookies++;
 					if (totalCookies % 100 == 0) {
-						multiplier *= 2;
+						multiplier += 1;
 					}
 				} else {
 					pw.setCircleSize(200, 200, cookies);
@@ -203,7 +126,11 @@ public class CookieFrameMain extends JFrame {
 			}
 			if (time - System.currentTimeMillis() < 0) {
 				time = System.currentTimeMillis() + 1000;
-				cookies += basecps;
+				cookies = cookies.add(BigInteger.valueOf((long) basecps));
+			}
+			if (minuteTime - System.currentTimeMillis() < 0) {
+				minuteTime = System.currentTimeMillis() + 64000;
+				basecps += slavesPerMinute * 3;
 			}
 			cookieLabel.setText(" " + cookies);
 		}
@@ -215,11 +142,174 @@ public class CookieFrameMain extends JFrame {
 	}
 
 	public boolean inShopButton(int x, int y) {
-		return (x > getWidth() / 4 && x < getWidth() / 4 + 100 && y < getHeight() / 2 + 48 && y > getHeight() / 2 - 16);
+		return (x > getWidth() / 4 && x < getWidth() / 4 + 120 && y < getHeight() / 2 + 48 && y > getHeight() / 2 + 16);
+	}
+
+	public boolean inPowerupButton(int x, int y) {
+		return (x > getWidth() / 4 * 3 - 120 && x < getWidth() / 4 * 3 && y < getHeight() / 2 + 48
+				&& y > getHeight() / 2 + 16);
 	}
 
 	public void paint(Graphics g) {
 		super.paint(g);
 		pw.repaint();
+	}
+
+	/*
+	 * ~~~ Deep Dark Code ~~~
+	 * 
+	 * YOU HAVE BEEN WARNED
+	 * 
+	 * TURN BACK NOW
+	 * 
+	 * ~~~ ~~~~ ~~~~ ~~~~ ~~~
+	 */
+
+	public JFrame initShop() {
+		JFrame shop = new JFrame("Shop");
+		shop.setTitle("Shop");
+		shop.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+		shop.setSize(1440, 822);
+		shop.setResizable(false);
+		shop.setBackground(new Color(34, 34, 34));
+		shop.setLayout(new GridLayout(4, 6));
+		/*
+		 * Note: These items are not to offend anyone in any way. This was a nice joke
+		 * among a group of friends. The purpose is to show how everything on this list
+		 * was legal AND acceptable-ish in the 1800s.
+		 * 
+		 * Child: Lack of child labor laws until 1938.
+		 * Slave: Slavery wasn't abolished until 1865.
+		 * Indentured Servant: Not abolished until 1917.
+		 * Sharecropper: Sharecropping lasted until the 1950s.
+		 * Overseer: Overseers were a part of slavery, so they weren't gone until 1865.
+		 * Enforcer: Weren't a thing in context, just needed something else to put.
+		 * Slave Catcher: Goal was to catch runaway slaves... also used until the abolition of slavery.
+		 * Sex Slave: Not uncommon among slaveholders in the South.
+		 */
+		JButton[] shopItems = { new JButton("Child ( " + 10 + " )"), new JButton("Slave ( " + 100 + " )"),
+				new JButton("Indentured Servant ( " + 1000 + " )"), new JButton("Sharecropper ( " + 10000 + " )"),
+				new JButton("Overseer ( " + 100000 + " )"), new JButton("Enforcer ( " + 1000000 + " )"),
+				new JButton("Slave Catcher ( " + 10000000 + " )"), new JButton("Sex Slave ( " + 100000000 + " )") };
+
+		for (int i = 0; i < shopItems.length; i++) {
+			shopItems[i].setFocusable(false);
+			shopItems[i].addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					String sourceText = ((JButton) e.getSource()).getText();
+					String[] sourceInfo = sourceText.replaceAll("\\( (.*?) \\)", "-$1").replaceAll(" ", "").split("-");
+					BigInteger cookieCost = new BigInteger(sourceInfo[1]);
+					String cookieName = sourceInfo[0];
+
+					switch (cookieName.toLowerCase()) {
+					case "child":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							basecps += 1;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(2)));
+						}
+						((JButton) e.getSource()).setText("Child ( " + cookieCost + " )");
+						break;
+					case "slave":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							basecps += 3;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(4)));
+						}
+						((JButton) e.getSource()).setText("Slave ( " + cookieCost + " )");
+						break;
+					case "indenturedservant":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							basecps += 9;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(8)));
+						}
+						((JButton) e.getSource()).setText("Indentured Servant ( " + cookieCost + " )");
+						break;
+					case "sharecropper":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							basecps += 27;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(16)));
+						}
+						((JButton) e.getSource()).setText("Sharecropper ( " + cookieCost + " )");
+						break;
+					case "overseer":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							basecps += 81;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(32)));
+						}
+						((JButton) e.getSource()).setText("Overseer ( " + cookieCost + " )");
+						break;
+					case "enforcer":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							basecps += 243;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(32)));
+						}
+						((JButton) e.getSource()).setText("Enforcer ( " + cookieCost + " )");
+						break;
+					case "slavecatcher":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							slavesPerMinute += 1;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(32)));
+						}
+						((JButton) e.getSource()).setText("Slave Catcher ( " + cookieCost + " )");
+						break;
+					case "sexslave":
+						if (cookies.compareTo(cookieCost) >= 0) {
+							cookies = cookies.subtract(cookieCost);
+							slavesPerMinute += 5;
+							cookieCost = cookieCost.add(cookieCost.divide(BigInteger.valueOf(32)));
+						}
+						((JButton) e.getSource()).setText("Sex Slave ( " + cookieCost + " )");
+						break;
+					}
+
+				}
+			});
+		}
+		JLabel l = new JLabel("Cookies: ", SwingConstants.RIGHT);
+		cookieLabel = new JLabel(" " + cookies, SwingConstants.LEFT);
+		/* Row 1 */
+		shop.add(new JPanel());
+		shop.add(new JPanel());
+		shop.add(l);
+		shop.add(cookieLabel);
+		shop.add(new JPanel());
+		shop.add(new JPanel());
+		/* Row 2 */
+		shop.add(new JPanel());
+		shop.add(shopItems[0]);
+		shop.add(shopItems[1]);
+		shop.add(shopItems[2]);
+		shop.add(shopItems[3]);
+		shop.add(new JPanel());
+		/* Row 3 */
+		shop.add(new JPanel());
+		shop.add(shopItems[4]);
+		shop.add(shopItems[5]);
+		shop.add(shopItems[6]);
+		shop.add(shopItems[7]);
+		shop.add(new JPanel());
+		/* Row 4 */
+		shop.add(new JPanel());
+		shop.add(new JPanel());
+		shop.add(new JPanel());
+		shop.add(new JPanel());
+		shop.add(new JPanel());
+		shop.add(new JPanel());
+
+		shop.addWindowListener(new java.awt.event.WindowAdapter() {
+			@Override
+			public void windowClosing(WindowEvent windowEvent) {
+				shop.setVisible(false);
+				setVisible(true);
+			}
+		});
+
+		return shop;
 	}
 }
--- cookie-clicker/src/main/PanelWrapper.java
+++ cookie-clicker/src/main/PanelWrapper.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.math.BigInteger;
 import java.math.RoundingMode;
 import java.text.DecimalFormat;
 
@@ -11,7 +12,8 @@ import javax.swing.JPanel;
 
 public class PanelWrapper extends JPanel {
 	private static final long serialVersionUID = 1L;
-	int width, height, totalCookies;
+	int width, height;
+	BigInteger totalCookies = BigInteger.valueOf(0);
 	BufferedImage cookie;
 	String string;
 	double cps;
@@ -21,33 +23,35 @@ public class PanelWrapper extends JPanel {
 		width = 200;
 		height = 200;
 		this.cookie = cookie;
-		totalCookies = 0;
 		cps = 0.0;
 		df.setRoundingMode(RoundingMode.CEILING);
 	}
 
-	public void setCircleSize(int w, int h, int cookies) {
+	public void setCircleSize(int w, int h, BigInteger cookies) {
 		width = w;
 		height = h;
 		totalCookies = cookies;
 	}
-	
+
 	public void setCPS(double cps, double basecps) {
 		this.cps = cps + basecps;
 	}
-	
+
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 		g.drawImage(cookie, getWidth() / 2 - (width / 2), getHeight() / 2 - (height / 2), width, height, null);
 		g.setColor(Color.WHITE);
-		g.setFont(new Font("TrebuchetMS", Font.PLAIN, 32)); 
+		g.setFont(new Font("TrebuchetMS", Font.PLAIN, 32));
 		string = "" + totalCookies;
-		g.drawString(string, getWidth() / 2 - g.getFontMetrics().stringWidth(string)/2, getHeight() / 2 + 300);
+		g.drawString(string, getWidth() / 2 - g.getFontMetrics().stringWidth(string) / 2, getHeight() / 2 + 300);
 		string = "Cookies Per Second: " + df.format(cps);
-		g.drawString(string, getWidth() / 2 - g.getFontMetrics().stringWidth(string)/2, getHeight() / 2 - 300);
+		g.drawString(string, getWidth() / 2 - g.getFontMetrics().stringWidth(string) / 2, getHeight() / 2 - 300);
 		string = "Shop";
-		g.drawRect(getWidth()/4, getHeight()/2, 100, 32);
-		g.setFont(new Font("TrebuchetMS", Font.PLAIN, 25)); 
-		g.drawString(string, getWidth() / 4+23, getHeight() / 2+25);
+		g.drawRect(getWidth() / 4, getHeight() / 2, 120, 32);
+		g.drawRect(getWidth() / 4 * 3 - 120, getHeight() / 2, 120, 32);
+		g.setFont(new Font("TrebuchetMS", Font.PLAIN, 25));
+		g.drawString(string, getWidth() / 4 + 33, getHeight() / 2 + 25);
+		string = "Powerups";
+		g.drawString(string, getWidth() / 4 * 3 - 120 + 6, getHeight() / 2 + 25);
 	}
 }
